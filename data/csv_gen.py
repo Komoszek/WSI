c13764e1d3114fce17910699bfbcdd361ebd58b8
kowalakk <konradkowalak@gmail.com>
2022-05-19 11:50:53 +0200

    więcej chorób, poprawiona lista objawów

--- data/csv_gen.py
+++ data/csv_gen.py
@@ -7,12 +7,11 @@ def probableFalse(probability = 95):
     return random.randrange(1, 101) > probability
 
 class Disease:
-    def __init__(self, bol_brzucha, bol_gardla, bol_glowy, bol_miesni, bol_stawow, 
-    bol_w_klatce, dreszcze, dusznosci, goraczka, kaszel, niezyt_nosa, nudnosci, 
-    obrzek_konczyn, zaburzenia_nastroju, zgaga, zmeczenie, zmiana_masy, czy_chory):
-        self.list = [bol_brzucha, bol_gardla, bol_glowy, bol_miesni, bol_stawow, 
-    bol_w_klatce, dreszcze, dusznosci, goraczka, kaszel, niezyt_nosa, nudnosci, 
-    obrzek_konczyn, zaburzenia_nastroju, zgaga, zmeczenie, zmiana_masy, czy_chory]
+    def __init__(self, biegunka, bol_brzucha, bol_gardla, bol_glowy, bol_miesni, 
+    bol_stawow, bol_w_klatce, dusznosci, goraczka, kaszel, nudnosci, obrzek_konczyn, 
+    zaburzenia_nastroju, zgaga, zmeczenie, zmiana_masy, zmiany_skorne, internistyczna):
+        self.list = [biegunka, bol_brzucha, bol_gardla, bol_glowy, bol_miesni, bol_stawow, bol_w_klatce, dusznosci, goraczka, kaszel, nudnosci, obrzek_konczyn,zaburzenia_nastroju, zgaga, zmeczenie, 
+        zmiana_masy, zmiany_skorne, internistyczna]
     def genRow(self):
         row = []
         for symptom in self.list:
@@ -22,23 +21,42 @@ class Disease:
 def makeCSVFile(path, rowsNumber, delimiter=','):
     with open(path, 'w', newline='\n') as csvfile:
         writer = csv.writer(csvfile, delimiter=delimiter)
-        header = ["bol brzucha", "bol gardla", "bol glowy", "bol miesni", "bol stawow", 
-        "bol w klatce piersiowej", "dreszcze", "dusznosci", "goraczka", "kaszel", 
-        "niezyt nosa", "nudnosci", "obrzek konczyn", "zaburzenia nastroju", "zgaga", 
-        "zmeczenie", "nagla zmiana masy ciala", "chory?"]
+        header = ["biegunka", "bol brzucha", "bol gardla", "bol glowy", "bol miesni", "bol stawow", 
+        "bol w klatce piersiowej", "dusznosci", "goraczka", "kaszel", 
+        "nudnosci", "obrzek konczyn", "zaburzenia nastroju", "zgaga", 
+        "zmeczenie", "nagla zmiana masy ciala", "zmiany skorne", "choroba internistyczna?"]
         writer.writerow(header)
 
-        grypa = Disease(False, True, True, True, True, False, True, False, True,
-            True, True, False, False, False, False, True, False, True)
-        angina = Disease(False, True, True, False, True, False, False, False, True,
-            True, False, False, False, False, False, True, False, True)
-
-        diseases = [grypa, angina]
+        grypa = Disease(False, False, True, True, True, True, False, False, True,
+            True, False, False, False, False, True, False, False, True)
+        angina = Disease(False, False, True, True, False, True, False, False, True,
+            True, False, False, False, False, True, False, False, True)
+        astma = Disease(False, False, False, False, False, False, True, True, False, 
+            True, False, False, True, True, False, False, False, True )
+        choroby_serca = Disease(False, False, False, False, False, False, True, True, 
+            False, False, False, True, False, False, True, False, False, True)
+        choroby_tarczycy = Disease(True, False, False, False, False, False, False, True, 
+            False, False, False, False, True, False, True, True, False, True)
+        choroby_pokarmowe = Disease(True, True, False, False, False, False, True, False, 
+            False, False, True, False, False, True, False, True, False, True)
+        choroby_krwi = Disease(False, True, False, True, False, True, False, True, True, 
+            False, True, False, False, False, True, True, True, True)
+        reumatyzm = Disease(False, False, False, False, False, True, False, False, False, 
+            False, False, True, False, False, False, False, False, True)
+        # inne chroby
+        choroby_psychiczne = Disease(False, False, False, False, False, False, False, False, 
+            False, False, True, False, True, False, True, True, False, False)
+        alergie = Disease(False, True, False, False, False, False, False, True, False, True, 
+            False, False, False, False, False, False, True, False)
+        nowotwory = Disease(True, True, False, False, False, False, False, False, True, False, 
+            False, False, False, True, True, True, True, False)
+        diseases = [grypa, angina, astma, choroby_serca, choroby_tarczycy, choroby_pokarmowe,
+            choroby_krwi, reumatyzm, choroby_psychiczne, alergie, nowotwory]
         for _ in range(rowsNumber):
             writer.writerow(diseases[random.randrange(0, len(diseases))].genRow())
         csvfile.close()
 
-makeCSVFile("test.csv", 5)
+makeCSVFile("test.csv", 20)
 
 
 
