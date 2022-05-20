a3f798e8dfbeeb1a27fe13f8856cc259248502e9
kowalakk <konradkowalak@gmail.com>
2022-05-20 14:07:21 +0200

    Poprawki Łukasza

--- rulefinder/indMatrix.py
+++ rulefinder/indMatrix.py
@@ -1,30 +1,34 @@
 import numpy as np
 
-def indMatrix(atrMatrix, decArray):
-    atrCount = atrMatrix.shape[1]
-    unique, counts = np.unique(decArray, return_counts=True)
-    columns = counts[0]
-    rows = counts[1]
-    indMatrix = [[None] * (columns + 1) for _ in range(rows + 1)]
-    true_iter = 1
-    false_iter = 1
-
-    for i in range(len(decArray)):
-        if(decArray[i]):
-            indMatrix[true_iter][0] = i
-            true_iter += 1
+def indist(decMatrix):
+    rowCount = decMatrix.shape[0]
+    atrCount = decMatrix.shape[1] - 1
+    trueArray = []
+    falseArray = []
+    for i in range(0, rowCount):
+        if decMatrix[i,-1]:
+            trueArray.append(i)
         else:
-            indMatrix[0][false_iter] = i
-            false_iter += 1
+            falseArray.append(i)
 
-    for row in range(1, rows + 1):
-        for col in range(1, columns + 1):
+    rows = len(trueArray)
+    cols = len(falseArray)
+    indMatrix = [[None] * cols for _ in range(rows)]
+
+    for row in range(0, rows):
+        for col in range(0, cols):
             indMatrix[row][col] = [False] * atrCount
             for i in range(atrCount):
-                if(atrMatrix[indMatrix[row][0]][i] ^ atrMatrix[indMatrix[0][col]][i]):
+                if(decMatrix[trueArray[row]][i] ^ decMatrix[falseArray[col]][i]):
                     indMatrix[row][col][i] = True
 
-    return indMatrix
+    return indMatrix, trueArray, falseArray
+
+decMatrix = np.array([  [True, True, True],
+                        [True, False, False],
+                        [False, False, False],
+                        [False, True, False]])
+print(indist(decMatrix))
 
 
 
